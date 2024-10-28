81696739b35ab0acb45300906feae5b0f3e661e7
Obed Vazquez <obed.vazquez@gmail.com>
2024-10-28 05:14:53 -0600

    RIOT's API support added.
    Added support for new RIOT account information provided in the summoner data @ endpoint `/lol-summoner/v1/current-summoner` reflected in the DTO `LolSummonerSummoner.java` file and class.

--- src/main/java/generated/LolSummonerSummoner.java
+++ src/main/java/generated/LolSummonerSummoner.java
@@ -1,9 +1,14 @@
 package generated;
 
+/**
+ * League Client endpoint `/lol-summoner/v1/current-summoner` response.
+ */
+@SuppressWarnings("unused")
 public class LolSummonerSummoner {
 
 	public Long accountId;
 	public String displayName;
+	public String gameName;
 	public String internalName;
 	public Boolean nameChangeFlag;
 	public Integer percentCompleteForNextLevel;
@@ -13,6 +18,7 @@ public class LolSummonerSummoner {
 	public LolSummonerSummonerRerollPoints rerollPoints;
 	public Long summonerId;
 	public Integer summonerLevel;
+	public String tagLine;
 	public Boolean unnamed;
 	public Long xpSinceLastLevel;
 	public Long xpUntilNextLevel;
